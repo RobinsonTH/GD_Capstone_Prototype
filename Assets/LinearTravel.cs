25164235f6f6063a89bd57315ee488f316ef349a
Travis Robinson <travis96@gmail.com>
2025-08-12 18:57:45 -0400

    Added lifetime to arrows

--- Assets/LinearTravel.cs
+++ Assets/LinearTravel.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LinearProjectile : MonoBehaviour
 {
     public float speed;
+    public float lifetime;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        Destroy(gameObject, lifetime);
     }
 
     // Update is called once per frame
